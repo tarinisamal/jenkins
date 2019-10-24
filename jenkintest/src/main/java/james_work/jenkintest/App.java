1451ba9b026ca7385d800a9c2f5528157571564d
Tarini Samal <tarini.samal@gmail.com>
2019-10-24 19:58:28 +0530

    Commit to Local GIT

---
+++ jenkintest/src/main/java/james_work/jenkintest/App.java
@@ -0,0 +1,13 @@
+package james_work.jenkintest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
